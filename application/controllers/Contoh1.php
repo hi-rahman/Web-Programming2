f67c50966a650a1adb3fcf0352cc144c62db573d
hi-rahman <rahmannurhadi75@gmail.com>
2024-04-19 09:58:37 +0700

    Upload all files

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Rahman Nurhadi. saya tinggal di daerah Bekasi Utara, olahraga yang saya suka adalah bulu tangkis.";
+    }
+}
